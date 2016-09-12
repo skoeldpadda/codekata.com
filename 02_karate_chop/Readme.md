c47e6694c6daccf2ef5bb91955254c8bca4d6789
glindner <uldkl@student.kit.edu>
2016-09-12 10:57:25 +0200

    karate chop: iterative implementation

---
+++ 02_karate_chop/Readme.md
@@ -0,0 +1,17 @@
+#Kata 02: Karate Chop
+
+##Goals:
+* Encountered errors
+* Merits: which one will make it into production? fun of writing? hardest to get working?
+* how did you come up with approaches four and five?
+
+##Technique 1: iterative approach
+###Notes
+* many cases to consider and to explicitly solve
+* code duplication, eg for first loop iteration
+* hard to write, because of the many cases
+* handling the current search window is nasty and a possible source of errors
+
+###Encountered errors
+1. infinite loop: typo (two distinct variable names by accident)
+2. errenous handling of current search window
---
+++ 02_karate_chop/chop.py
@@ -0,0 +1,53 @@
+import math
+import unittest
+
+def chop_iterative(value, ints):
+    lowerBound = 0
+    upperBound = len(ints)    #exclusive
+        
+    while True:
+        numElements = upperBound - lowerBound
+
+        if numElements == 0:
+            return -1
+        elif numElements == 1:
+            return lowerBound if ints[lowerBound] == value else -1
+        
+        pivotIndex = lowerBound + math.floor(numElements / 2.0)
+        pivotValue = ints[pivotIndex]
+        
+        if pivotValue == value:
+            return pivotIndex
+        elif pivotValue < value:
+            lowerBound = pivotIndex + 1
+        elif pivotValue > value:
+            upperBound = pivotIndex
+
+class TestChopping(unittest.TestCase):
+    def test_iterative(self):
+        self.chopTestTemplate(chop_iterative)
+    
+    def chopTestTemplate(self, fun):
+        self.assertEqual(-1, fun(3, []))
+        self.assertEqual(-1, fun(3, [1]))
+        self.assertEqual(0,  fun(1, [1]))
+        #
+        self.assertEqual(0,  fun(1, [1, 3, 5]))
+        self.assertEqual(1,  fun(3, [1, 3, 5]))
+        self.assertEqual(2,  fun(5, [1, 3, 5]))
+        self.assertEqual(-1, fun(0, [1, 3, 5]))
+        self.assertEqual(-1, fun(2, [1, 3, 5]))
+        self.assertEqual(-1, fun(4, [1, 3, 5]))
+        self.assertEqual(-1, fun(6, [1, 3, 5]))
+        #
+        self.assertEqual(0,  fun(1, [1, 3, 5, 7]))
+        self.assertEqual(1,  fun(3, [1, 3, 5, 7]))
+        self.assertEqual(2,  fun(5, [1, 3, 5, 7]))
+        self.assertEqual(3,  fun(7, [1, 3, 5, 7]))
+        self.assertEqual(-1, fun(0, [1, 3, 5, 7]))
+        self.assertEqual(-1, fun(2, [1, 3, 5, 7]))
+        self.assertEqual(-1, fun(4, [1, 3, 5, 7]))
+        self.assertEqual(-1, fun(6, [1, 3, 5, 7]))
+        self.assertEqual(-1, fun(8, [1, 3, 5, 7]))
+
+unittest.main()
